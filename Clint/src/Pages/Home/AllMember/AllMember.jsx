a088ccb4985f5b60fe99c8a8176f984fdc9326ad
jishan1019 <imranislamjishan200@gmail.com>
2023-12-02 21:11:32 +0600

    search and filter design add

--- Clint/src/Pages/Home/AllMember/AllMember.jsx
+++ Clint/src/Pages/Home/AllMember/AllMember.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 
 const AllMember = () => {
-  return <div>All Memeber</div>;
+  return (
+    <div>
+      {/* Searchbar */}
+      <div className="searchFilterContainer flex justify-between items-center gap-2">
+        <div className="search flex items-center w-[75%] lg:w-[87%]">
+          <input
+            className="w-full py-[8.5px] px-4 border-black border"
+            type="text"
+            name=""
+            id=""
+            placeholder="Enter clints name"
+          />
+
+          <button className="bgPrimary text-white py-[9.5px] px-3">
+            Search
+          </button>
+        </div>
+
+        <select className="select select-info w-[30%] lg:w-[13%]">
+          <option disabled selected>
+            Filter
+          </option>
+          <option>Domain</option>
+          <option>Gender</option>
+          <option>Availability</option>
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default AllMember;
--- Clint/src/Pages/Home/Home.jsx
+++ Clint/src/Pages/Home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <div className="chart lg:w-[25%] w-full bg-white">
         <ChartInfo />
       </div>
-      <div className="member hidden lg:block w-[75%] h-screen bg-[#DDDDDD]">
+      <div className="member hidden lg:block w-full lg:w-[75%] h-screen bg-[#DDDDDD] px-2 lg:px-6 py-5">
         <AllMember />
       </div>
     </div>
