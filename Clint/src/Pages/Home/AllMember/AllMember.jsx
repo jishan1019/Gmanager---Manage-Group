e2e3f1b4d6bffd2eb60a0495ba77d3084eb00b60
jishan1019 <imranislamjishan200@gmail.com>
2023-12-02 22:11:07 +0600

    card data dinamic show

--- Clint/src/Pages/Home/AllMember/AllMember.jsx
+++ Clint/src/Pages/Home/AllMember/AllMember.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-const AllMember = () => {
+const AllMember = ({ users }) => {
+  const userData = users;
+
   return (
     <div>
       {/* Searchbar */}
@@ -27,6 +29,52 @@ const AllMember = () => {
           <option>Gender</option>
           <option>Availability</option>
         </select>
+      </div>
+
+      {/* User Card */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12 px-2 lg: px-0">
+        {userData?.map((user, index) => (
+          <div key={index} className="card w-full bg-base-100 shadow-xl">
+            <figure>
+              <img
+                className="h-16 w-16 mt-3 rounded-full border"
+                src={user?.avatar}
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                Name: {user?.first_name} {user?.last_name}{" "}
+              </h2>
+              <p className="text-xs ">
+                <span className="font-semibold">Email:</span> {user?.email}
+              </p>
+              <p className="text-xs ">
+                {" "}
+                <span className="font-semibold">Gender:</span> {user?.gender}
+              </p>
+              <p className="text-xs ">
+                <span className="font-semibold">Domain:</span> {user?.domain}
+              </p>
+              <p className="text-xs ">
+                <span className="font-semibold">Available:</span>{" "}
+                {user?.available ? "Available" : "Unavailable"}
+              </p>
+
+              <div className="btnAdd">
+                {user?.available ? (
+                  <p className="bg-[#26a69a] text-center text-white mt-3 py-2 cursor-pointer hover:bg-green-500">
+                    Add Group
+                  </p>
+                ) : (
+                  <p className="bg-red-500 text-center text-white mt-3 py-2 cursor-not-allowed ">
+                    Unavailable
+                  </p>
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- Clint/src/Pages/Home/Home.jsx
+++ Clint/src/Pages/Home/Home.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AllMember from "./AllMember/AllMember";
 import ChartInfo from "./ChartInfo/ChartInfo";
 
 const Home = () => {
+  const [users, setUser] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:4000/user");
+        const data = await response.json();
+        setUser(data.slice(0, 10));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(users);
+
   return (
     <div className="flex">
-      <div className="chart lg:w-[25%] w-full bg-white">
+      <div className="chart  lg:w-[25%] w-full bg-white">
         <ChartInfo />
       </div>
-      <div className="member hidden lg:block w-full lg:w-[75%] h-screen bg-[#DDDDDD] px-2 lg:px-6 py-5">
-        <AllMember />
+      <div className="member hidden lg:block w-full lg:w-[75%] min-h-screen  bg-[#DDDDDD] px-2 lg:px-6 py-5">
+        <AllMember users={users} />
       </div>
     </div>
   );
